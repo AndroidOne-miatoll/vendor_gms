a45f669a4a807238a3d2524986a88e7913ab1188
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-04-18 18:36:25 +0000

    voice: Import offline voice recognition models
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- setup-gms.mk
+++ setup-gms.mk
@@ -7,3 +7,5 @@ $(call inherit-product, vendor/gms/custom-gms/setup-custom-gms.mk)
 # Build GMS overlays
 $(call inherit-product, vendor/gms/overlays/setup-overlays.mk)
 
+# Build offline voice recognition models
+$(call inherit-product, vendor/gms/voice/voice-vendor.mk)
