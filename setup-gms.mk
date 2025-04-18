85a0ba42bfb027fffb8f4a148f8df7cd20ff3c07
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-04-18 17:40:37 +0000

    overlays: Import the GMS overlays
    Directly imported from [1]
    
    ref [1] : https://gitlab.com/pixelos-aosp/vendor_gms
    
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- setup-gms.mk
+++ setup-gms.mk
@@ -4,3 +4,6 @@ $(call inherit-product, vendor/gms/partner-gms/partner-gms-vendor.mk)
 # Build custom-gms
 $(call inherit-product, vendor/gms/custom-gms/setup-custom-gms.mk)
 
+# Build GMS overlays
+$(call inherit-product, vendor/gms/overlays/setup-overlays.mk)
+
