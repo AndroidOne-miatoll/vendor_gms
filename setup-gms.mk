ea9d8b1496b4792879e720f8e35525eb5c38e3d2
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-04-18 18:49:41 +0000

    gms: Add some GMS services related props
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- setup-gms.mk
+++ setup-gms.mk
@@ -12,3 +12,39 @@ $(call inherit-product, vendor/gms/voice/voice-vendor.mk)
 
 # Build Pixel Sounds
 $(call inherit-product, vendor/gms/media/media-vendor.mk)
+
+# Default ringtone/notification/alarm sounds
+PRODUCT_PRODUCT_PROPERTIES += \
+	ro.config.ringtone=The_big_adventure.ogg \
+    ro.config.notification_sound=Popcorn.ogg \
+    ro.config.alarm_alert=Bright_morning.ogg
+
+# Gboard Props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.bs_theme=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
+
+# GMS Props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true
+
+# SetupWizard Props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.esim_cid_ignore=00000001 \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.portal_notification=true \
+    setupwizard.feature.enable_quick_start_flow=true \
+    setupwizard.feature.enable_restore_anytime=true \
+    setupwizard.feature.enable_wifi_tracker=true \
+    setupwizard.feature.lifecycle_refactoring=true \
+    setupwizard.feature.notification_refactoring=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.show_support_link_in_deferred_setup=false \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
+    setupwizard.personal_safety_suw_enabled=true \
+    setupwizard.theme=glif_v4_light
+
