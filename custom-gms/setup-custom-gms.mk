f36eead8be251a74a8d65d7667542943af9cb80f
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-08-22 12:06:06 +0000

    custom-gms: Import splited GMS applications
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

---
+++ custom-gms/setup-custom-gms.mk
@@ -0,0 +1,10 @@
+# Build custom-gms
+PRODUCT_PACKAGES += \
+	Maps \
+	Photos \
+	YouTube \
+	PrebuiltGmail \
+	DevicePersonalizationPrebuiltPixel2023-bfinal_aiai_20250217.00_RC08 \
+	PrebuiltBugle \
+	PrebuiltGmsCoreVic \
+	Velvet
